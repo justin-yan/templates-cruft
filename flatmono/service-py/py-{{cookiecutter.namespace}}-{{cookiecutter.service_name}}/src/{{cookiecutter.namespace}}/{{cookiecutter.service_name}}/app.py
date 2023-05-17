ba5af9d3d1aa5ed7293fe1120cef8578c400d8fa
Justin <justin@yan.family>
2023-05-17 10:15:20 -0700

    Adding openapi customization

--- flatmono/service-py/py-{{cookiecutter.namespace}}-{{cookiecutter.service_name}}/src/{{cookiecutter.namespace}}/{{cookiecutter.service_name}}/app.py
+++ flatmono/service-py/py-{{cookiecutter.namespace}}-{{cookiecutter.service_name}}/src/{{cookiecutter.namespace}}/{{cookiecutter.service_name}}/app.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
+from functools import lru_cache
 
 from {{cookiecutter.namespace}}.{{cookiecutter.service_name}}.ports.api.health import health_router
 
@@ -9,5 +11,16 @@ def create_app() -> FastAPI:
     ## Route Specification
     ########
     app.include_router(health_router)
+
+
+    @lru_cache()
+    def custom_openapi():
+        return get_openapi(
+            title="{{cookiecutter.service_name}}",
+            version="0.0.1",
+            description="",
+            routes=app.routes,
+        )
+    app.openapi = custom_openapi
 
     return app
