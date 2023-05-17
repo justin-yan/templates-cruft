90a8657c1c332638b9a1e9c5b652af7c5910149c
Justin <justin@yan.family>
2023-05-17 10:37:34 -0700

    Adding a config instantiation object

--- flatmono/service-py/py-{{cookiecutter.namespace}}-{{cookiecutter.service_name}}/src/{{cookiecutter.namespace}}/{{cookiecutter.service_name}}/app.py
+++ flatmono/service-py/py-{{cookiecutter.namespace}}-{{cookiecutter.service_name}}/src/{{cookiecutter.namespace}}/{{cookiecutter.service_name}}/app.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI
 from fastapi.openapi.utils import get_openapi
 from functools import lru_cache
 
+from {{cookiecutter.namespace}}.{{cookiecutter.service_name}}.infrastructure.config import Config
 from {{cookiecutter.namespace}}.{{cookiecutter.service_name}}.ports.api.health import health_router
 
 def create_app() -> FastAPI:
     app = FastAPI()
+    config = Config()
 
     ########
     ## Route Specification
@@ -16,9 +18,9 @@ def create_app() -> FastAPI:
     @lru_cache()
     def custom_openapi():
         return get_openapi(
-            title="{{cookiecutter.service_name}}",
-            version="0.0.1",
-            description="",
+            title=config.app_name,
+            version=config.version,
+            description=config.environment,
             routes=app.routes,
         )
     app.openapi = custom_openapi
---
+++ flatmono/service-py/py-{{cookiecutter.namespace}}-{{cookiecutter.service_name}}/src/{{cookiecutter.namespace}}/{{cookiecutter.service_name}}/infrastructure/config.py
@@ -0,0 +1,6 @@
+from pydantic import BaseSettings
+
+class Config(BaseSettings):
+    app_name: str = "{{cookiecutter.service_name}}"
+    version: str = "0.0.1"
+    environment: str = "dev"
